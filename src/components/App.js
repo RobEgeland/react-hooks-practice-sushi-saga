dd1af451710f06ddeed2be1176a051f8def91229
Robert Egeland <robegeland@yahoo.com>
2022-03-23 19:45:42 -0500

    commit tree 886521895134cb0d64f31b5aff2b4d5fa5beb123

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushi, setSushi] = useState([])
+  const [moneyLeft, setMoneyLeft] = useState(100)
+  const [plates, setPlates] = useState([])
+
+  function changeMoneyAndPlates(price, name) {
+    setMoneyLeft(moneyLeft - price);
+    const newPlatesArr = [...plates, name]
+    setPlates(newPlatesArr)
+  
+  }
+
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(sushi => setSushi(sushi))
+  },[])
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+    <SushiContainer moneyLeft={moneyLeft} changeMoneyAndPlates={changeMoneyAndPlates} sushi={sushi} />
+      <Table plates={plates} moneyLeft={moneyLeft} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushi, changeMoneyAndPlates, moneyLeft}) {
+  const [firstSlice, setFirstSlice] = useState(0)
+  const [secondSlice, setSecondSlice] = useState(4)
+  const sushiChunk = sushi.slice(firstSlice, secondSlice)
+
+  function changeDisplay() {
+    setFirstSlice((firstSlice) => firstSlice + 4);
+    setSecondSlice((secondSlice) => secondSlice + 4);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiChunk.map((roll) => <Sushi moneyLeft={moneyLeft} changeMoneyAndPlates={changeMoneyAndPlates} key={roll.id} name={roll.name} img={roll.img_url} price={roll.price} />)}
+      <MoreButton changeDisplay={changeDisplay} />
     </div>
   );
 }
